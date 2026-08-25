85bcd709b8df7cc9f4bb8f775c69aa8253504396
Haru-26 <pp7415393@gmail.com>
2026-08-25 21:41:43 +0530

    Add interactive orbital visualization

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -2,4 +2,4 @@ import MissionViewport from '../components/MissionViewport'
 import MonitoringConsole from '../components/MonitoringConsole'
 import { mockThreats } from '../data/mockMissionData'
 import { useMissionContext } from '../layouts/useMissionContext'
-export default function DashboardPage() { const { selectedThreat, setSelectedThreat } = useMissionContext(); return <><MissionViewport selectedThreat={selectedThreat} /><MonitoringConsole threats={mockThreats} selectedThreat={selectedThreat} onSelect={setSelectedThreat} /></> }
+export default function DashboardPage() { const { selectedThreat, setSelectedThreat } = useMissionContext(); const selectObject = id => { const threat = mockThreats.find(item => item.objectA === id || item.objectB === id); if (threat) setSelectedThreat(threat) }; return <><MissionViewport selectedThreat={selectedThreat} onObjectSelect={selectObject} /><MonitoringConsole threats={mockThreats} selectedThreat={selectedThreat} onSelect={setSelectedThreat} /></> }
